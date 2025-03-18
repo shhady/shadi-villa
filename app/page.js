1d8dd622a08d791f3b5c92b4d885fe9319ddae69
shhady <shhadyse@gmail.com>
2025-03-19 01:32:13 +0200

    new first

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                  src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+                  src={'/home.jpg'}
                   alt="Villa and pool"
                   width={2432}
                   height={1442}
